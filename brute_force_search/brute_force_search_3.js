03dd138d31b453edde98dd39d3f6bb019a0560b2
jiwonkim <luvgy@naver.com>
2024-01-12 00:39:21 +0900

    완전탐색 문제 추가

--- brute_force_search/brute_force_search_3.js
+++ brute_force_search/brute_force_search_3.js
@@ -1,1 +1,39 @@
 // lv.2 소수 찾기
+
+function solution(numbers) {
+  var answer = 0;
+  
+  // 소수 판단
+  function sosu(num) {
+      let cnt = 0;
+      if (num===0 || num===1) return false;
+      for (let i=1; i<=num; i++) {
+          if (num%i===0) cnt++;
+      }
+      if (cnt===2) return true;
+      else return false;
+  }
+  
+  // 순열
+  const permutation = new Set();
+
+  makePermutation('', numbers);
+
+  // 모든 순열 구하기
+  function makePermutation(perm, others) {
+      if (perm !== '') {
+          permutation.add(Number(perm));
+      }
+      for (let i=0; i < others.length; i++) {
+          const remove_i_others = others.substr(0,i) + others.substr(i+1);
+          makePermutation(perm + others[i], remove_i_others);
+      }
+  }
+  
+  // 소수 개수 세기
+  for (let item of permutation) {
+      if (sosu(item)) answer++;
+  }
+
+  return answer;
+}
--- brute_force_search/brute_force_search_7.js
+++ brute_force_search/brute_force_search_7.js
@@ -1,1 +1,13 @@
 // lv.2 모음사전
+
+function solution(word) {
+  const vowels = ["A", "E", "I", "O", "U"];
+  const counts = [781, 156, 31, 6, 1]; // 각 자리수별 가능한 단어 수
+
+  let answer = 0;
+  for (let i = 0; i < word.length; i++) {
+    answer += vowels.indexOf(word[i]) * counts[i] + 1; // 해당 자리의 알파벳 인덱스에 따른 영향을 더해줌
+  }
+
+  return answer;
+}
